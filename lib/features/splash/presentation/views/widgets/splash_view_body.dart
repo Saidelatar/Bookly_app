757d63b0d0dd2d2513d8c617d14ffa88a628771f
Saidelatar <srmee1526@gmail.com>
2024-02-07 20:30:10 +0200

    refactor init state & navigate to home view

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,76 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding1_Text.dart';
+import 'package:bookly_app/features/splash/presentation/views/widgets/sliding2_text.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({
+    super.key,
+  });
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> sliding1Animation;
+  late Animation<Offset> sliding2Animation;
+
+  @override
+  void initState() {
+    super.initState();
+    initSlidingAnimation();
+
+    navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Center(
+          child: Sliding1Animation(sliding1Animation: sliding1Animation),
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Sliding2Animation(slidingAnimation: sliding2Animation)
+      ],
+    );
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+
+    sliding1Animation = Tween<Offset>(
+      begin: const Offset(2, 0),
+      end: Offset.zero,
+    ).animate(animationController);
+    animationController.forward();
+
+    sliding2Animation = Tween<Offset>(
+      begin: const Offset(0, 5),
+      end: Offset.zero,
+    ).animate(animationController);
+  }
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 4), () {
+      Get.to(HomeView(), transition: Transition.fade, duration: kTransition);
+    });
+  }
+}
