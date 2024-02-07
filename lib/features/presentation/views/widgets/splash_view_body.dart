34375339bb15d57f5939352047a2472c1cd144ae
Saidelatar <srmee1526@gmail.com>
2024-02-07 17:03:06 +0200

    splash view ui done

--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -1,8 +1,21 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({
+    super.key,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Center(
+          child: Image.asset(AssetsData.logo),
+        )
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -13,6 +14,8 @@ class Bookly extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      theme: ThemeData().copyWith(scaffoldBackgroundColor: kprimaryColor),
+      debugShowCheckedModeBanner: false,
       home: SplashView(),
     );
   }
