6326540016a81bb4ee650e222d7ff7106dacd20f
Saidelatar <srmee1526@gmail.com>
2024-02-07 21:50:51 +0200

    add custom list view item

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,4 +1,5 @@
 class AssetsData {
   static const String logo = "assets/images/Logo.png";
+  static const String testimage = 'assets/images/R.jpeg';
   static const String searchSvg = "assets/images/magnifying-glass-solid.svg";
 }
