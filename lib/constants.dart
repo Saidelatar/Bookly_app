853e5647115f306ceb29a71ff14d2634bf3de4ac
Saidelatar <srmee1526@gmail.com>
2024-02-08 19:30:32 +0200

    add fonts styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kprimaryColor = Color(0xff100B20);
+const String kGtSectraFine = 'GT Sectra Fine';
 const kTransition = Duration(
   microseconds: 500,
 );
