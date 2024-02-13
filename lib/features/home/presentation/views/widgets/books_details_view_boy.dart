f234c8971d162342f0b8023d62dc152981485b2a
Saidelatar <srmee1526@gmail.com>
2024-02-13 20:30:26 +0200

    create similar books list view

--- lib/features/home/presentation/views/widgets/books_details_view_boy.dart
+++ lib/features/home/presentation/views/widgets/books_details_view_boy.dart
@@ -1,8 +1,9 @@
 import 'package:bookly_app/core/utils/styles.dart';
-import 'package:bookly_app/core/utils/widgets/custom_button.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/books_rating.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/button_action.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_books_details_view_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/feature_books_item.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/similar_books_list_view.dart';
 import 'package:flutter/material.dart';
 
 class BooksDetailsViewBody extends StatelessWidget {
@@ -49,39 +50,24 @@ class BooksDetailsViewBody extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
           ),
           const ButtonAction(),
-        ],
-      ),
-    );
-  }
-}
-
-class ButtonAction extends StatelessWidget {
-  const ButtonAction({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8),
-      child: Row(
-        children: [
-          CustomButton(
-            text: '19.99 £',
-            textColor: Colors.black,
-            backgroundColor: Colors.white,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(16),
-              bottomLeft: Radius.circular(16),
+          const SizedBox(
+            height: 50,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle14.copyWith(
+                fontWeight: FontWeight.w600,
+              ),
             ),
           ),
-          CustomButton(
-            text: 'Free Preview',
-            fontSize: 16,
-            textColor: Colors.white,
-            backgroundColor: Color(0xffef8262),
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(16),
-              bottomRight: Radius.circular(16),
-            ),
+          const SizedBox(
+            height: 16,
+          ),
+          const SimilarBooksListView(),
+          const SizedBox(
+            height: 40,
           ),
         ],
       ),
