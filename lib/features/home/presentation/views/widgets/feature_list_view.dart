8c9c77580fa74e23662d2da380acc87b14a456de
Saidelatar <srmee1526@gmail.com>
2024-02-17 23:36:58 +0200

    display featured books part one

--- lib/features/home/presentation/views/widgets/feature_list_view.dart
+++ lib/features/home/presentation/views/widgets/feature_list_view.dart
@@ -1,21 +1,35 @@
+import 'package:bookly_app/core/utils/widgets/custom_error_widget.dart';
+import 'package:bookly_app/core/utils/widgets/custom_loading_indicator.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/feature_books_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeaturedBooksListView extends StatelessWidget {
   const FeaturedBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.25,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) {
-            return const Padding(
-              padding: EdgeInsets.only(right: 10),
-              child: FeaturedBooksImage(),
-            );
-          }),
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+      builder: (context, state) {
+        if (state is FeaturedBooksSuccessed) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.25,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.only(right: 10),
+                    child: FeaturedBooksImage(),
+                  );
+                }),
+          );
+        } else if (state is FeaturedBooksFauilre) {
+          return CustomErrorWidget(errorMessage: state.errorMessage);
+        } else {
+          return const Center(child: CustomLoadingIndicator());
+        }
+      },
     );
   }
 }
