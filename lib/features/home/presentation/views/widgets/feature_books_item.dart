8f12edd05a9ef709299df463fc8c7413332fee15
Saidelatar <srmee1526@gmail.com>
2024-02-18 00:32:43 +0200

    cashed network image

--- lib/features/home/presentation/views/widgets/feature_books_item.dart
+++ lib/features/home/presentation/views/widgets/feature_books_item.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class FeaturedBooksImage extends StatelessWidget {
@@ -6,15 +7,14 @@ class FeaturedBooksImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.7 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(14),
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(imageUrl),
-          ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
       ),
     );
